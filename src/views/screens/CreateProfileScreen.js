dfd43bf18f41d00a2fd472b8ca9a184a94c9b805
HalaSaadeh <halagsaadeh@gmail.com>
2022-04-03 00:23:46 +0300

    fixed carousel

--- src/views/screens/CreateProfileScreen.js
+++ src/views/screens/CreateProfileScreen.js
@@ -15,248 +15,187 @@ const { width, height } = Dimensions.get("window");
 
 import COLORS from "../../consts/colors";
 import CustomInput from "../components/CustomInput";
-
-const slides = [
-  {
-    type: "PersonalDetails", // full name
-    title: "What should we call you?",
-    description: "Your name will be shown to other users on the app.",
-  },
-  {
-    type: "BirthdayDetails", // birthday
-    title: "When were you born?",
-    description:
-      "This information is kept private but will be used to match you with others.",
-  },
-  {
-    type: "LocationDetails", // country, city,
-    title: "Where are you from?",
-    description: "Your location helps us direct you to the right places.",
-  },
-  {
-    type: "ProfileDetails", // photo and bio
-    title: "Customize your profile.",
-    description:
-      "Help others know more about you by adding a bio and profile picture. You can always skip and come back later.",
-  },
-  {
-    type: "InterestDetails", //list of interests to check
-    title: "What are your interests?",
-    description: "Choose between 3 to 5 interests for optimal recommendations.",
-  },
-];
-
-const Slide = ({ item }) => {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  switch (item.type) {
-    case "PersonalDetails":
-      return (
-        <SafeAreaView style={styles.root}>
-          <Text style={styles.headerTitle}>{item?.title}</Text>
-          <Text style={styles.subtitle}>{item?.description}</Text>
-          <Text style={styles.inputDesc}>First Name</Text>
-          <CustomInput
-            placeholder="First Name"
-            value={firstName}
-            setValue={setFirstName}
-          />
-          <Text style={styles.inputDesc}>Last Name</Text>
-          <CustomInput
-            placeholder="Last Name"
-            value={lastName}
-            setValue={setLastName}
-          />
-        </SafeAreaView>
-      );
-    case "BirthdayDetails":
-        return (
-          <View style={styles.root}>
-            <Text style={styles.headerTitle}>{item?.title}</Text>
-            <Text style={styles.subtitle}>{item?.description}</Text>
-          </View>
-        );
-    case "LocationDetails":
-      return (
-        <View style={styles.root}>
-          <Text style={styles.headerTitle}>{item?.title}</Text>
-          <Text style={styles.subtitle}>{item?.description}</Text>
-        </View>
-      );
-    case "ProfileDetails":
-      return (
-        <View style={styles.root}>
-          <Text style={styles.headerTitle}>{item?.title}</Text>
-          <Text style={styles.subtitle}>{item?.description}</Text>
-        </View>
-      );
-    case "InterestDetails":
-      return (
-        <View style={styles.root}>
-          <Text style={styles.headerTitle}>{item?.title}</Text>
-          <Text style={styles.subtitle}>{item?.description}</Text>
-        </View>
-      );
-    default:
-      return null;
-  }
-};
+import Carousel from "pinar";
 
 const CreateProfileScreen = ({ navigation }) => {
-  const [currentSlideIndex, setCurrentSlideIndex] = React.useState(0);
-  const ref = React.useRef();
-  const updateCurrentSlideIndex = (e) => {
-    const contentOffsetX = e.nativeEvent.contentOffset.x;
-    const currentIndex = Math.round(contentOffsetX / width);
-    setCurrentSlideIndex(currentIndex);
-  };
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [carousel, setCarousel] = useState(null);
 
   const goToNextSlide = () => {
-    const nextSlideIndex = currentSlideIndex + 1;
-    if (nextSlideIndex != slides.length) {
-      const offset = nextSlideIndex * width;
-      ref?.current.scrollToOffset({ offset });
-      setCurrentSlideIndex(currentSlideIndex + 1);
+    if (carousel !== null) {
+      carousel.scrollToNext();
     }
   };
 
-  const goToPreviousSlide = () => {
-    const nextSlideIndex = currentSlideIndex - 1;
-    if (nextSlideIndex != -1) {
-      const offset = nextSlideIndex * width;
-      ref?.current.scrollToOffset({ offset });
-      setCurrentSlideIndex(nextSlideIndex);
+  const goToPrevSlide = () => {
+    if (carousel !== null) {
+      carousel.scrollToPrev();
     }
-  };
-
-  const Footer = () => {
-    return (
-      <View
-        style={{
-          height: height * 0.25,
-          justifyContent: "space-between",
-          paddingHorizontal: 20,
-        }}
-      >
-        {/* Indicator container */}
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "center",
-            marginTop: 20,
-          }}
-        >
-          {/* Render indicator */}
-          {slides.map((_, index) => (
-            <View
-              key={index}
-              style={[
-                styles.indicator,
-                currentSlideIndex == index && {
-                  backgroundColor: COLORS.primary,
-                  width: 25,
-                },
-              ]}
-            />
-          ))}
-        </View>
-
-        {/* Render buttons */}
-        <View style={{ marginBottom: 20 }}>
-          {currentSlideIndex == slides.length - 1 ? (
-            <View style={{ height: 50 }}>
-              <TouchableOpacity
-                style={styles.btn}
-                onPress={() => navigation.replace("LoginScreen")}
-              >
-                <Text
-                  style={{
-                    fontWeight: "bold",
-                    fontSize: 15,
-                    color: COLORS.white,
-                  }}
-                >
-                  GET STARTED
-                </Text>
-              </TouchableOpacity>
-            </View>
-          ) : (
-            <View style={{ flexDirection: "row" }}>
-              <TouchableOpacity
-                activeOpacity={0.8}
-                style={[
-                  styles.btn,
-                  {
-                    borderColor: COLORS.primary,
-                    borderWidth: 1,
-                    backgroundColor: "transparent",
-                  },
-                ]}
-                onPress={goToPreviousSlide}
-              >
-                <Text
-                  style={{
-                    fontWeight: "bold",
-                    fontSize: 15,
-                    color: COLORS.primary,
-                  }}
-                >
-                  BACK
-                </Text>
-              </TouchableOpacity>
-              <View style={{ width: 15 }} />
-              <TouchableOpacity
-                activeOpacity={0.8}
-                onPress={goToNextSlide}
-                style={styles.btn}
-              >
-                <Text
-                  style={{
-                    fontWeight: "bold",
-                    fontSize: 15,
-                    color: COLORS.white,
-                  }}
-                >
-                  NEXT
-                </Text>
-              </TouchableOpacity>
-            </View>
-          )}
-        </View>
-      </View>
-    );
   };
 
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.white }}>
+    <View style={styles.rootView}>
       <StatusBar backgroundColor={COLORS.primary} />
-      <FlatList
-        ref={ref}
-        onMomentumScrollEnd={updateCurrentSlideIndex}
-        contentContainerStyle={{ height: height * 0.75 }}
-        showsHorizontalScrollIndicator={false}
-        horizontal
-        data={slides}
-        pagingEnabled
-        renderItem={({ item }) => <Slide item={item} />}
-        keyExtractor={(item, index) => index.toString()}
-      />
-      <Footer />
-    </SafeAreaView>
+      <Carousel
+        showsControls={false}
+        ref={(c) => {
+          setCarousel(c);
+        }}
+      >
+        <View style={styles.root}>
+          <SafeAreaView style={styles.safeRoot}>
+            <Text>Let's create your profile...</Text>
+            <Text style={styles.headerTitle}>What should we call you?</Text>
+            <Text style={styles.subtitle}>
+              Your name will be shown to other users on the app.
+            </Text>
+            <Text style={styles.inputDesc}>First Name</Text>
+            <CustomInput
+              placeholder="First Name"
+              value={firstName}
+              setValue={setFirstName}
+            />
+            <Text style={styles.inputDesc}>Last Name</Text>
+            <CustomInput
+              placeholder="Last Name"
+              value={lastName}
+              setValue={setLastName}
+            />
+          </SafeAreaView>
+        </View>
+        <View style={styles.root}>
+          <SafeAreaView style={styles.safeRoot}>
+            <Text style={styles.headerTitle}>When were you born?</Text>
+            <Text style={styles.subtitle}>
+              This information is kept private but will be used to match you
+              with others.
+            </Text>
+          </SafeAreaView>
+        </View>
+        <View style={styles.root}>
+          <SafeAreaView style={styles.safeRoot}>
+            <Text style={styles.headerTitle}>Where are you from?</Text>
+            <Text style={styles.subtitle}>
+              Your location helps us direct you to the right places.
+            </Text>
+          </SafeAreaView>
+        </View>
+        <View style={styles.root}>
+          <SafeAreaView style={styles.safeRoot}>
+            <Text style={styles.headerTitle}>Customize your profile.</Text>
+            <Text style={styles.subtitle}>
+              Help others know more about you by adding a bio and profile
+              picture. You can always skip and come back later.
+            </Text>
+          </SafeAreaView>
+        </View>
+        <View style={styles.root}>
+          <SafeAreaView style={styles.safeRoot}>
+            <Text style={styles.headerTitle}>What are your interests?</Text>
+            <Text style={styles.subtitle}>
+              Choose between 3 to 5 interests for optimal recommendations.
+            </Text>
+          </SafeAreaView>
+        </View>
+      </Carousel>
+      {/* <View style = {{
+        paddingLeft: "10%",
+        paddingRight: "10%",
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "flex-start",
+        width: "100%",
+        backgroundColor: COLORS.white}}>
+        {carousel !== null && carousel.index >= Number(4) ? (
+          <View style={{ height: 50 }}>
+            <TouchableOpacity
+              style={styles.btn}
+              onPress={() => navigation.replace("LoginScreen")}
+            >
+              <Text
+                style={{
+                  fontWeight: "bold",
+                  fontSize: 15,
+                  color: COLORS.white,
+                }}
+              >
+                GET STARTED
+              </Text>
+            </TouchableOpacity>
+          </View>
+        ) : (
+          <View style={{ flexDirection: "row" }}>
+            <TouchableOpacity
+              activeOpacity={0.8}
+              style={[
+                styles.btn,
+                {
+                  borderColor: COLORS.primary,
+                  borderWidth: 1,
+                  backgroundColor: "transparent",
+                },
+              ]}
+              onPress={goToPrevSlide}
+            >
+              <Text
+                style={{
+                  fontWeight: "bold",
+                  fontSize: 15,
+                  color: COLORS.primary,
+                }}
+              >
+                BACK
+              </Text>
+            </TouchableOpacity>
+            <View style={{ width: 15 }} />
+            <TouchableOpacity
+              activeOpacity={0.8}
+              onPress={goToNextSlide}
+              style={styles.btn}
+            >
+              <Text
+                style={{
+                  fontWeight: "bold",
+                  fontSize: 15,
+                  color: COLORS.white,
+                }}
+              >
+                NEXT
+              </Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      </View> */}
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  root: {
-    paddingTop: "20%",
-    paddingLeft: "10%",
-    paddingRight: "20%",
-    paddingBottom: "35%",
+  rootView: {
     flex: 1,
     justifyContent: "center",
     alignItems: "flex-start",
     width: "100%",
-    resizeMode: "contain",
+    backgroundColor: COLORS.white,
+  },
+  root: {
+    paddingTop: "20%",
+    paddingBottom: "20%",
+    paddingLeft: "10%",
+    paddingRight: "10%",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "flex-start",
+    width: "100%",
+    backgroundColor: COLORS.white,
+  },
+  safeRoot: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "flex-start",
+    width: "100%",
+    backgroundColor: COLORS.white,
   },
   headerTitle: {
     color: COLORS.primary,
